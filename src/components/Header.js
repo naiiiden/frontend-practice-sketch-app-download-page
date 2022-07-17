34700c60a71b6ce73ab4c3fea57db3e727c0ca30
naiiiden <na4ko359@gmail.com>
2022-07-17 17:18:03 +0300

    mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,14 @@ const Header = () => {
             <img src={logo} alt="Sketch's logo" className="logo"/>
             <nav className="header--nav">
                 <a href="#" className="sign-in--link">Sign In</a>
+                <ul className={`header--nav--ul ${openMenu ? "show" : ""}`}>
+                    <li>Product</li>
+                    <li>Learn</li>
+                    <li><a href="#">Apps</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li>Support</li>
+                    <li><a href="#">Get started for free</a></li>
+                </ul>
             </nav>
         </header>
     )
