07211cfdeb0ca916619df3f461aa5af8aa1ea620
naiiiden <na4ko359@gmail.com>
2022-07-17 16:02:36 +0300

    import images from og website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./style/general.css";
+import testimg from "./images/desktop/webapp-image@4x.avif";
 
 const App = () => {
   return (
-    <p>hey</p>
-  )
+    <>
+      <p>hey</p>
+      <img src={testimg}/>
+    </>
+    )
 }
 
 export default App;
