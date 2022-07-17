76764560c23fb9e8b980b226d0e674544fbf5b2f
naiiiden <na4ko359@gmail.com>
2022-07-17 16:40:27 +0300

    header styled on mobile+tablet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import logo from "../images/logo.svg";
+import menu from "../images/menu.svg";
 
 const Header = () => {
     return (
-        <header>
-            <p>hey</p>
-            <img src={logo}/>
+        <header className="header">
+            <button aria-label="toggle navigation">
+                <span className="menu--toggle--span"></span>
+                <span className="menu--toggle--span"></span>
+            </button>
+            <img src={logo} alt="Sketch's logo" className="logo"/>
+            <nav className="header--nav">
+                <a href="#" className="sign-in--link">Sign In</a>
+            </nav>
         </header>
     )
 }
