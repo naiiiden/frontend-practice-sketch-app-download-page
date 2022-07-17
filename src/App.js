60309b1729a92ecad90fee92608b29d57175ad0a
naiiiden <na4ko359@gmail.com>
2022-07-17 16:11:23 +0300

    set body background color

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./style/general.css";
-import testimg from "./images/desktop/webapp-image@4x.avif";
 
 const App = () => {
   return (
-    <>
       <p>hey</p>
-      <img src={testimg}/>
-    </>
     )
 }
 
