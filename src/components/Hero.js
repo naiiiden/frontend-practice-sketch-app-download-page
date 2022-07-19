f7dfff997e1ecbca1d7a56dc3c3c38721637841a
naiiiden <na4ko359@gmail.com>
2022-07-19 21:12:37 +0300

    hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+const Hero = () => {
+    return (
+        <section className="hero--section">
+            <h1 className="hero--heading">
+                <span>Every</span>
+                <span>hero</span>
+                <span>needs</span>
+                <span>a</span>
+                <span>sidekick.</span>
+                <span>We</span>
+                <span>have</span>
+                <span>two.</span>
+            </h1>
+            <p>Create with our award-winning macOS app, share your projects in the browser (on any platform), and take Sketch with you.</p>
+        </section>
+    )
+}
+
+export default Hero;
