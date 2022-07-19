f78582770b80f9bd7ea1a428cf0dcb54a625da71
naiiiden <na4ko359@gmail.com>
2022-07-19 21:25:31 +0300

    hero component styled on mobile

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./style/general.css";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <div>
       <Header/>
       <main>
-        
+        <Hero/>
       </main>
     </div>
     )
