7e5aba6b6e5fcc03c6be7d148d7432195e3d8c7a
naiiiden <na4ko359@gmail.com>
2022-07-20 22:09:57 +0300

    features component styled on mobile

--- src/components/Features.js
+++ src/components/Features.js
@@ -13,12 +13,12 @@ const Features = () => {
         <section className="features--container">
             <div className="mac--container">
                 <img src={macAppMobileSmall} alt="" className="mac--small"/>
-                <img src={macAppMobileLarge} alt="" className="mac-large"/>
+                <img src={macAppMobileLarge} alt="" className="mac--large"/>
                 <img src={macAppDesktop} alt="" className="mac--desktop"/>
                 <div className="text--wrapper">
-                    <p>FOR EDITORS</p>
+                    <p className="for">FOR EDITORS</p>
                     <h2>Design on your Mac</h2>
-                    <p>Design, prototype, and illustrate in a truly native macOS app, online or offline. Work in private or collaborate with others, in real time.</p>
+                    <p className="desc">Design, prototype, and illustrate in a truly native macOS app, online or offline. Work in private or collaborate with others, in real time.</p>
                     <a href="https://www.sketch.com/downloads/mac/" className="blackened--link">Download for macOS</a>
                     <div className="links--container">
                         <a href="https://www.sketch.com/updates/">Browse old versions</a>
@@ -30,10 +30,10 @@ const Features = () => {
                 <img src={webAppMobileSmall} alt="" className="web--small"/>
                 <img src={webAppMobileLarge} alt="" className="web--large"/>
                 <img src={webAppDesktop} alt="" className="web--desktop"/>
-                <div className="text--wraper">
-                    <p>FOR EVERYONE</p>
+                <div className="text--wrapper">
+                    <p className="for">FOR EVERYONE</p>
                     <h2>Explore in your browser</h2>
-                    <p>Browse documents, inspect designs, export assets, discuss feedback, manage users, and invite guests - on any computer, in any browser.</p>
+                    <p className="desc">Browse documents, inspect designs, export assets, discuss feedback, manage users, and invite guests - on any computer, in any browser.</p>
                     <a href="https://www.sketch.com/c" className="blackened--link">Open the web app</a>
                 </div>
             </div>
@@ -42,11 +42,11 @@ const Features = () => {
                 <img src={iphoneAppMobileLarge} alt="" className="iphone--large"/>
                 <img src={iphoneAppDesktop} alt="" className="iphone--desktop"/>
                 <div className="text--wrapper">
-                    <p>FOR EVERYONE</p>
+                    <p className="for">FOR EVERYONE</p>
                     <h2>Preview on iPhone</h2>
-                    <p>View your documents on the go, play prototypes on a real device, and mirror designs as you edit them on your Mac.</p>
+                    <p className="desc">View your documents on the go, play prototypes on a real device, and mirror designs as you edit them on your Mac.</p>
                     <a href="https://apps.apple.com/app/sketch-view-and-mirror/id1609224699" className="blackened--link">Download on the App Store</a>
-                    <p>Our iPhone app works exclusively with Workspace documents.</p>
+                    <p className="iphone--desc">Our iPhone app works exclusively with Workspace documents.</p>
                 </div>
             </div>
         </section>
